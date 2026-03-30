eaa73eb3a64d9fae4c0573f6f9949ed7e3445a5c
Andromeda <stroev.andrew@gmail.com>
2026-03-30 03:34:08 +0100

    A whole new build, that's what we'll see...

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Andromeda's CV";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          texEnv = pkgs.texliveSmall.withPackages (ps: with ps; [
+            latexmk
+            # latexsym and tabularx are in tools (included in texliveSmall)
+            preprint   # fullpage
+            titlesec
+            marvosym
+            enumitem
+            fancyhdr
+            babel
+            ec         # T1/EC fonts required by babel-english
+            cm-super   # Type1 versions of EC fonts for PDF output
+            # glyphtounicode is part of the base distribution
+          ]);
+        in
+        {
+          default = pkgs.stdenvNoCC.mkDerivation {
+            pname = "andromeda-cv";
+            version = "0.1.0";
+
+            src = ./src;
+
+            nativeBuildInputs = [ texEnv ];
+
+            buildPhase = ''
+              export TEXMFHOME="$TMPDIR/texmf"
+              latexmk -pdf -interaction=nonstopmode cv.tex
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              cp cv.pdf $out/andromeda-cv.pdf
+            '';
+          };
+        }
+      );
+    };
+}
